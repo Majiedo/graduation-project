44767677a119bfc399b446bc147793e4310efb10
Majed <70872870+Majiedo@users.noreply.github.com>
2024-12-08 09:33:58 +0300

    latest changes

--- react-application/src/pages/Logs.tsx
+++ react-application/src/pages/Logs.tsx
@@ -4,7 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Logs = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-y-5 pt-10 font-mono">
+    <div className="w-full flex flex-col items-center justify-center gap-y-5 pt-10 font-mono mb-20">
       <Tabs defaultValue="logs" className="px-2 w-[90%]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="logs">Logs</TabsTrigger>
